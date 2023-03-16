21e468a0db42c3a3bb2b9c60a4e06bdd8614109b
“Rarindotme” <rarin@rarin.me>
2023-03-16 13:18:20 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints "With proper grammer,
+ *                but the outcome is a piece of art.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        printf("\"With proper grammar, but the outcome is a piece of art.");
+
+        return (0);
+}
+
