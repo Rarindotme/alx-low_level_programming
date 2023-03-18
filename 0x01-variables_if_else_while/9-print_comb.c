5796b40af828362360e4aabd919fda6f6c29ff2d
“Rarindotme” <rarin@rarin.me>
2023-03-18 07:21:20 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point print all possible combinations
+ * of digit numbers
+ *
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+	{	putchar((x % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
