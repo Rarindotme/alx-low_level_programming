308bf642aa2546b8e831f407ffe221497e872d6d
“Rarindotme” <rarin@rarin.me>
2023-03-27 09:48:25 -0700

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,10 +9,11 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str);
+void _puts(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
