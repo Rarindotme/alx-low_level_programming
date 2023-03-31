2191095578c1d2b11abd180688be4c906041c4c8
“Rarindotme” <rarin@rarin.me>
2023-03-31 08:27:40 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: char
+ * @src: char
+ * @n: integer
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int l = 0;
+
+	while (src[i++])
+		l++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = l; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
